7f81700df1184470aefd2d6acf64dc2b9bc0e4fd
Luem <luchemma@gmail.com>
2023-06-16 04:24:13 -0300

    feature: normalizeData (camelCasing the response)

--- src/utils/normalizeProp.js
+++ src/utils/normalizeProp.js
@@ -3,3 +3,19 @@ export function normalizeProp(prop) {
         return match[1].toUpperCase()
     })
 }
+
+export function normalizeData(data) {
+    if (Array.isArray(data)) return data.map((obj) => normalizeData(obj))
+    else if (typeof data === 'object') {
+        return Object.keys(data).reduce((acc, key) => {
+            const camelCaseKey = normalizeProp(key)
+            const value = data[key]
+
+            acc[camelCaseKey] = value ? normalizeData(value) : null
+
+            return acc
+        }, {})
+    }
+
+    return data
+}
