0cc9481aed525819084b1614331390b31e024565
Luem <luchemma@gmail.com>
2023-06-16 04:23:45 -0300

    feature: getFlight service & utils barrel

--- src/endpoint/services.js
+++ src/endpoint/services.js
@@ -1,10 +1,35 @@
 import { prisma } from '../config.js'
+import { CustomError } from '../utils/customError.js'
+import { normalizeData } from '../utils/normalizeProp.js'
 
-export function getAllFlights() {
-    return prisma.flight.findMany({
+export async function getFlight(paramsId) {
+    const id = Number(paramsId)
+
+    if (typeof id !== 'number')
+        throw new CustomError('The id is required, and must be a number', 400)
+
+    const flight = await prisma.flight.findUnique({
+        where: {
+            flight_id: id,
+        },
         include: {
-            aircraft: true,
-            airport: true,
+            boarding_pass: {
+                include: {
+                    seat: true,
+                    passenger: {
+                        select: {
+                            dni: true,
+                            name: true,
+                            age: true,
+                            country: true,
+                        },
+                    },
+                },
+            },
         },
     })
+
+    if (!flight) throw new CustomError('Flight not found', 404)
+
+    return normalizeData(flight)
 }
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export * from './airplane.js'
+export * from './customError.js'
+export * from './normalizeProp.js'
