50c35497b5afe38a2185f9c870ea49a7188bdd2a
Luem <luchemma@gmail.com>
2023-06-18 03:53:39 -0300

    move middlewares to index folder and service logic into server.js

---
+++ src/middlewares.js
@@ -0,0 +1,91 @@
+import cors from 'cors'
+import morgan from 'morgan'
+import { json as expressJSON, urlencoded } from 'express'
+import { prisma } from './config.js'
+import { CustomError } from './utils/customError.js'
+import { normalizeData } from './utils/normalizeProp.js'
+
+export const serverMiddlewares = [
+    cors({
+        origin: '*',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: [
+            'Content-Type',
+            'Origin',
+            'X-Requested-With',
+            'Accept',
+        ],
+    }),
+    morgan('dev'),
+    expressJSON({
+        limit: '50mb',
+    }),
+    urlencoded({
+        extended: true,
+        limit: '50mb',
+    }),
+]
+
+export async function checkFlight(req, _res, next) {
+    try {
+        const id = Number(req.params.id)
+
+        if (typeof id !== 'number')
+            throw new CustomError(
+                'The id is required, and must be a number',
+                400
+            )
+
+        const flight = await prisma.flight.findUnique({
+            where: {
+                flight_id: id,
+            },
+            include: {
+                boarding_pass: {
+                    include: {
+                        seat: true,
+                        passenger: {
+                            select: {
+                                dni: true,
+                                name: true,
+                                age: true,
+                                country: true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+
+        if (!flight) throw new CustomError('Flight not found', 404)
+
+        // Convierto las propiedades de la respuesta, de snake_case a camelCase, y lo paso al controlador por el objeto Request
+        req.flight = normalizeData(flight)
+
+        next()
+    } catch (error) {
+        next(error)
+    } finally {
+        prisma.$disconnect()
+    }
+}
+
+/* eslint-disable no-unused-vars */
+export function errorHandler(err, _req, res, _next) {
+    const { message, status } = err
+
+    if (status === 404) {
+        console.error(message)
+
+        res.status(status).send({
+            data: {},
+        })
+    } else {
+        console.error(message)
+
+        res.status(400).json({
+            errors: 'could not connect to db',
+        })
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import express from 'express'
 
-import { errorHandler, serverMiddlewares } from './endpoint/middlewares.js'
-import { controller as mainController } from './endpoint/controller.js'
+import { Airplane } from './models/Airplane.js'
+import { checkFlight, errorHandler, serverMiddlewares } from './middlewares.js'
 import { PORT } from './config.js'
 
 const app = express()
 
 app.use(serverMiddlewares)
 
-app.get('/flights/:id/passengers', mainController)
+app.get('/flights/:id/passengers', checkFlight, function (req, res) {
+    const { boardingPass, ...flight } = req.flight
+    const airplane = new Airplane(flight.flightId, boardingPass)
+
+    res.status(200).send({
+        data: {
+            ...flight,
+            passengers: airplane.getAllPassengers(),
+        },
+    })
+})
 
 app.use(errorHandler)
 
